3cb5379ca741fdc565225b97a0e52867c70ebe0b
Sujeet Kumar Singh <96869458+suankidev@users.noreply.github.com>
2024-07-04 10:21:00 +0530

    Update build.gradle.kts with Spark version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,11 @@ dependencies {
                           implementation("org.apache.commons:commons-text")
 
     implementation(project(":utilities"))
-}
+
+    implementation("org.apache.spark:spark-core_2.13:3.5.0")
+    implementation("org.apache.spark:spark-sql_2.13:3.5.0")
+
+}    
 
 
 
